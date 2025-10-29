73fe235bc71573164513660483a3fc40e41570a8
kswoo02 <tjsdnrla02@naver.com>
2025-10-29 10:53:59 +0900

    V

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 5000   
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ksw:8451@ksw.ye1kyd7.mongodb.net/?appName=ksw', {
-}).then(() => console.log("----- MongoDB connected -----"))
-  .catch(err => console.log(err));
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config/mongoURI, {})
+  .then(() => console.log("----- MongoDB connected -----"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({ success: false, err });
+  }
+});
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.post("/register", (req, res) => {
+//   const user = new User(req.body);
+
+//   user.save((err, userInfo) => {
+//     if (err) return res.json({ success: false, err });
+//     return res.status(200).json({
+//       success: true,
+//     });
+//   });
+// });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
